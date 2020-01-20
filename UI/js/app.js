70bda7d21bf1cea1a85c97ce2c4a3982130fa5d4
Richard NSHIMYUMUKIZA <ricahcyuzuzo@gmail.com>
2020-01-20 13:53:41 +0200

    Correcting login for access admin dashboard

--- UI/js/app.js
+++ UI/js/app.js
@@ -3,7 +3,9 @@ const closeNav = document.querySelector('#closeNav');
 const mobileMenu = document.querySelector('#mobileMenu');
 const joinUs = document.querySelector('#join-button');
 const createN = document.querySelector('#createNew');
-
+const form = document.querySelector('#form');
+// const email = document.querySelector('#email');
+// const password = document.querySelector('#password');
 
 // Creating event listeners
 
@@ -11,6 +13,7 @@ openNav.addEventListener('click', openNavi);
 closeNav.addEventListener('click', closeNavi);
 joinUs.addEventListener('click', signup);
 createN.addEventListener('click',createNewAnnouncement);
+
 
 
 function openNavi() {
@@ -30,6 +33,27 @@ function createNewAnnouncement(){
     window.location.href = '../html/create-announcement.html';
 }
 
+function checkUser() {
+    const email = document.getElementById('email').value;
+    const password = document.getElementById('password').value;
 
+    const users = [
+        {
+            email: 'admin@admin.com',
+            password: 'admin'
+        },
+        {
+            email: 'ricah@ricah.com',
+            password: 'ricah'
+        }
+    ];
 
-
+    for (let i = 0; i < users.length; i++) {
+        
+        if (email == users[i].email && password == users[i].password) {
+            window.location.href = '../html/admin-dashboard.html';
+        }else{
+            window.location.href = '../html/ad-dashboard.html';
+        }
+    }
+}
