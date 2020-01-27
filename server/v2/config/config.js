b07803a3daf9ca3287f1b25625fdefe412dc1b6e
Richard NSHIMYUMUKIZA <ricahcyuzuzo@gmail.com>
2020-01-27 17:26:23 +0200

    Connecting postgres database to API

---
+++ server/v2/config/config.js
@@ -0,0 +1,16 @@
+/* eslint-disable comma-dangle */
+import { Pool } from 'pg';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const isProduction = process.env.NODE_ENV === 'production';
+
+const connectionString = `postgresql://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_DATABASE}`;
+
+const pool = new Pool({
+  connectionString: isProduction ? process.env.DATABASE_URL : connectionString,
+  ssl: isProduction
+});
+
+export default pool;
