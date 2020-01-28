4fbbb0ec3c1a4d620f1dca2a32e2014ac0a6ab4c
Richard NSHIMYUMUKIZA <ricahcyuzuzo@gmail.com>
2020-01-28 09:40:58 +0200

    Making the same format of connection

--- server/v2/config/config.js
+++ server/v2/config/config.js
@@ -11,11 +11,11 @@ const isProduction = process.env.NODE_ENV === 'isProduction';
 
 if (process.env.NODE_ENV === 'isTesting') {
   pool = new Pool({
-    connectionString: process.env.TESTING
+    connectionString: process.env.TESTING_URL
   });
 } else if (process.env.NODE_ENV === 'isProduction') {
   pool = new Pool({
-    connectionString: process.env.PRODUCTION,
+    connectionString: process.env.PRODUCTION_URL,
     ssl: isProduction
   });
 } else {
